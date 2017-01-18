dd347d68d6b111bf4d5895668210cc1fc1f8ede8
Ryan Atkins <ryan@koda.agency>
2017-01-18 21:12:44 +0000

    add composer

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <html>
-	<head>
-		<title>MailGun Email Frontend</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <title>MailGun Email Frontend</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	</head>
-	<body>
-		<div class="container">
-		  <h1>MailGun Email Frontend</h1>
-			<form action="" method="post">
-  		  <label>
- 			    <p>Subject</p>
-				  <input type="text" name="emailSubject">
-			  </label>
-								
-				<button type="submit">Send Email</button>
-			</form>
-		</div>
-	</body>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1>MailGun Email Frontend</h1>
+      <form action="" method="post">
+        <fieldset class="">
+          <legend>Setup</legend>
+          <label>
+            <p>Subject</p>
+            <input type="text" name="emailSubject">
+          </label>
+        </fieldset>
+
+        <fieldset class="">
+        </fieldset>
+
+        <button type="submit">Send Email</button>
+      </form>
+    </div>
+  </body>
 </html>
 <?php
 
